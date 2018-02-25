3b15281fde9fd27f2be34f106ca22a5bd22c07d4
Bezoyan <zoya1905bezoyan@gmail.com>
2018-02-26 03:44:26 +0400

    second commit

--- js/user/index.js
+++ js/user/index.js
@@ -1,1 +1,12 @@
+var Users = React.createClass({
+    render: function () {
+        return (
+            <div className="users">
+                <h3>{this.props.children}</h3><hr />
+                <h3>{this.props.choldren}</h3><hr />
+            </div>
+        );
+    }
+});
 
+ReactDOM.render(Users, document.getElementById('users'));
